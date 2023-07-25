842ca1511008ac4eb89576afb4b68ddea7b18220
kcassube <79251426+kcassube@users.noreply.github.com>
2023-07-25 13:10:42 +0200

    Class_Lektion

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,9 +4,11 @@ public class App
 {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello World!");
     }
-    
+
 }
+    
+
 
 
